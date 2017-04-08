dd7169fb9fac49e5c5573a151a6b5fea003a3901
Devon Phillips <devonsphillips@hotmail.com>
2017-04-08 15:27:55 -0400

    first commit bruh

---
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a404dalmations.superstudentscheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
