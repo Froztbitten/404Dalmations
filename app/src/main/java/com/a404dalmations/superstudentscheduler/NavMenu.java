21ef18a2c5383c48a411fb169816421e4c6ca92c
Devon Phillips <devonsphillips@hotmail.com>
2017-04-08 16:25:38 -0400

    first commit bruh

--- app/src/main/java/com/a404dalmations/superstudentscheduler/NavMenu.java
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/NavMenu.java
@@ -1,21 +1,19 @@
 package com.a404dalmations.superstudentscheduler;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+public class NavMenu extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_menu);
     }
 
     public void navMenu(View view){
-        startActivity(new Intent(this, NavMenu.class));
     }
 }
