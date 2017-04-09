e02f6f0d7a9b61e96bb8d4764262652851bb9ce8
jonathan <jonathanbauman8@gmail.com>
2017-04-08 21:17:47 -0400

    add finantial class

--- app/src/main/java/com/a404dalmations/superstudentscheduler/Financial.java
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/Financial.java
@@ -14,6 +14,7 @@ public class Financial {
     private int hoursWorkPerWeek;
     //4.34524=weeks per mounth
     private float spendingMoniez;
+    public boolean hasFoodBeenCalculated;
 
 
     public Financial(float currentBalance, float rentPerMonth, float utilitiesSum, float priviousMonthUtilities, float payPerHour, int hoursWorkPerWeek, float spendingMoniez) {
@@ -128,6 +129,89 @@ public class Financial {
         setSpendingMoniez(avalibleFunds);
 
     }
+
+
+    public boolean canYouAffordFood()
+    {
+        //get the users amount needs for foods
+        float amountNeeds=0;
+        if(getSpendingMoniez()<21)
+        {
+            return false;
+            //the user is less then a ramen noodle budget;
+            //tell the user to go look under the coutch for loose change
+            //and buy some noodles
+            //RIP you health
+        }
+        else if(getSpendingMoniez()<amountNeeds)
+        {
+            //see how off the user is from affording the food they want
+            if(amountNeeds-getSpendingMoniez()<30)
+            {
+                return false;
+                //you are close to affording this food
+                //make small changes to your wants or try to scrape some cash togeather
+            }
+            else
+            {
+                return false;
+                //you need to rethink the food you can get
+                //you cannot afford food that expensive
+            }
+
+        }
+        else
+        {
+            setSpendingMoniez(getSpendingMoniez()-amountNeeds);
+            hasFoodBeenCalculated=true;
+            //you can afford food this week!!!!
+            //spending money avalibe has been updated
+            return true;
+        }
+    }
+
+    public boolean canYouAffordStuff()
+    {
+        //how much is the thing
+        float amountNeeds=0;
+        if(hasFoodBeenCalculated==false)
+        {
+            //no you cant afford food so you cant afford stuff
+            if(canYouAffordFood()==false)
+            {
+                return false;
+            }
+
+
+        }
+        if(hasFoodBeenCalculated==true)
+        {
+            if(getSpendingMoniez()<amountNeeds)
+            {
+                if(amountNeeds-getSpendingMoniez()<30)
+                {
+                    return false;
+                    //you clase to affording the thing
+                    //just try and scrap some cash together and you got it
+                }
+                else
+                {
+                    return false;
+                    //you cannot afford the thing so dont een dream of what it is like to have one
+                }
+            }
+            else
+            {
+                setSpendingMoniez(getSpendingMoniez()-amountNeeds);
+                return true;
+                //you can afford the thing!
+                //spending money is updated
+            }
+        }
+        else return false;
+    }
+
+
 
 
     public static void main(String[] args) {
