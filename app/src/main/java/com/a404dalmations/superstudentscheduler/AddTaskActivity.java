68883085a27cee0cbf48db8dd44748f9c01813f7
Jacob <jwa5426@psu.edu>
2017-04-09 06:04:31 -0400

    - Possibly got the remove course to work

--- app/src/main/java/com/a404dalmations/superstudentscheduler/AddTaskActivity.java
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/AddTaskActivity.java
@@ -1,11 +1,18 @@
 package com.a404dalmations.superstudentscheduler;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.a404dalmations.superstudentscheduler.Courses.Semester;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class AddTaskActivity extends AppCompatActivity
 {
@@ -17,7 +24,7 @@ public class AddTaskActivity extends AppCompatActivity
 
             /** This stuff will probably have to be updated to get and update the information correctly
              in the Json. **/
-            /*
+/*
             ArrayAdapter<String> adapter = new ArrayAdapter<String>
                     (this, android.R.layout.simple_spinner_item, person.hist.getSemesters.get(person.hist.getSemesters.size - 1).getCourses.getName);
 
--- app/src/main/java/com/a404dalmations/superstudentscheduler/RemoveCourseActivity.java
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/RemoveCourseActivity.java
@@ -1,9 +1,19 @@
 package com.a404dalmations.superstudentscheduler;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.a404dalmations.superstudentscheduler.Courses.History;
+import com.a404dalmations.superstudentscheduler.Courses.ScheduleActivity;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 
 public class RemoveCourseActivity  extends AppCompatActivity {
@@ -15,8 +25,26 @@ public class RemoveCourseActivity  extends AppCompatActivity {
 
         /** This stuff will probably have to be updated to get and update the information correctly
             in the Json. **/
-        /*
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, person.Hist.getSemesters().getSemesterNum());
+
+        setContentView(R.layout.remove_course);
+
+
+    }
+    public void removeCourse(View view)
+    {
+        SharedPreferences sharedPref = getSharedPreferences("name", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+
+        String json = sharedPref.getString("Person", "plz");
+        Person person = gson.fromJson(json, Person.class);
+        History history = person.getHistory();
+
+        int count = history.getSemesterCount();
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(int i = 1; i <= nums.size(); i++)
+            nums.add(i);
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nums);
 
         // Add the values from the array list to the spinner.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -26,8 +54,12 @@ public class RemoveCourseActivity  extends AppCompatActivity {
         // Get the number of the semester that is selected.
         int selectedSemester = (int)sItems.getSelectedItem();
 
-       adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, person.Hist.getSemesters().get(selectedSemester).getCourses().getCourseName);
+        count = history.getSemesters().get(selectedSemester - 1).getCourses().size();
+        ArrayList<String> names = new ArrayList<>();
+        for(int i = 0; i < nums.size(); i++)
+            names.add(history.getSemesters().get(selectedSemester - 1).getCourses().get(i).getName());
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, names);
 
         // Add the values from the array list to the spinner.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -36,17 +68,17 @@ public class RemoveCourseActivity  extends AppCompatActivity {
 
         // Get the name of the selected semester from the spinner.
         String selectedCourse = sItems.getSelectedItem().toString();
-        */
 
         /** Add the code to only do the next part when the button is selected. **/
         // Search the array list of courses in the selected semester to its index so that it can be
         // removed.
-        /*
-        for(int i = 0; i < person.Hist.getSemesters().get(selectedSemester).getCourse.size(); i ++)
+
+        for(int i = 0; i < history.getSemesters().get(selectedSemester).getCourses().size(); i ++)
         {
-            if(person.Hist.getSemesters.get(selectedSemester).getCourses.get(i) == selectedCourse)
-                person.Hist.getSemesters.get(selectedSemester).getCourses.remove(i);
+            if(history.getSemesters().get(selectedSemester).getCourses().get(i).getName() == selectedCourse)
+                history.getSemesters().get(selectedSemester).getCourses().remove(i);
         }
-        */
+
+        startActivity(new Intent(this, ScheduleActivity.class));
     }
 }
