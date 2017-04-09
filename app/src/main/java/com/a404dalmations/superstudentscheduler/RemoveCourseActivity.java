5f87d39531538be08f7825ef353a1225cb6cfed0
Jacob <jwa5426@psu.edu>
2017-04-09 08:26:38 -0400

    Finally got the drop course function to work. FINALLY

--- app/src/main/java/com/a404dalmations/superstudentscheduler/RemoveCourseActivity.java
+++ app/src/main/java/com/a404dalmations/superstudentscheduler/RemoveCourseActivity.java
@@ -1,13 +1,16 @@
 package com.a404dalmations.superstudentscheduler;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.a404dalmations.superstudentscheduler.Courses.History;
 import com.a404dalmations.superstudentscheduler.Courses.ScheduleActivity;
@@ -16,7 +19,7 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 
 
-public class RemoveCourseActivity  extends AppCompatActivity {
+public class RemoveCourseActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,7 +31,55 @@ public class RemoveCourseActivity  extends AppCompatActivity {
 
         setContentView(R.layout.remove_course);
 
+        SharedPreferences sharedPref = getSharedPreferences("name", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
 
+        String json = sharedPref.getString("Person", "plz");
+        Person person = gson.fromJson(json, Person.class);
+        History history = person.getHistory();
+
+        //int count = history.getSemesterCount();
+        int count = 1;
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(int i = 1; i <= count; i++)
+            nums.add(i);
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nums);
+
+        // Add the values from the array list to the spinner.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.semesterSpinner);
+
+        sItems.setAdapter(adapter);
+    }
+
+    public void selectSemester(View view)
+    {
+        SharedPreferences sharedPref = getSharedPreferences("name", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+
+        String json = sharedPref.getString("Person", "plz");
+        Person person = gson.fromJson(json, Person.class);
+        History history = person.getHistory();
+        // Get the number of the semester that is selected.
+
+        Spinner sItems = (Spinner) findViewById(R.id.semesterSpinner);
+        //sItems.setOnItemSelectedListener(this);
+        int selectedSemester = (int)sItems.getSelectedItem();
+        //int count = history.getSemesterCount();
+
+        int count = history.getSemesters().get(selectedSemester - 1).getCourses().size();
+
+        ArrayList<String> names = new ArrayList<>();
+        for(int j = 0; j < count; j++)
+            names.add(history.getSemesters().get(selectedSemester - 1).getCourses().get(j).getName());
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, names);
+
+        // Add the values from the array list to the spinner.
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sItems = (Spinner) findViewById(R.id.courseSpinner);
+        sItems.setAdapter(adapter1);
     }
     public void removeCourse(View view)
     {
@@ -39,45 +90,29 @@ public class RemoveCourseActivity  extends AppCompatActivity {
         Person person = gson.fromJson(json, Person.class);
         History history = person.getHistory();
 
-        int count = history.getSemesterCount();
-        ArrayList<Integer> nums = new ArrayList<>();
-        for(int i = 1; i <= count; i++)
-            nums.add(i);
-
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nums);
-
-        // Add the values from the array list to the spinner.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner sItems = (Spinner) findViewById(R.id.semesterSpinner);
-        sItems.setAdapter(adapter);
-        
-        // Get the number of the semester that is selected.
-        int selectedSemester = (int)sItems.getSelectedItem();
-
-        count = history.getSemesters().get(selectedSemester - 1).getCourses().size();
-        ArrayList<String> names = new ArrayList<>();
-        for(int i = 0; i < count; i++)
-            names.add(history.getSemesters().get(selectedSemester - 1).getCourses().get(i).getName());
-
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, names);
-
-        // Add the values from the array list to the spinner.
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sItems = (Spinner) findViewById(R.id.semesterSpinner);
-        sItems.setAdapter(adapter1);
+        Spinner semesterItems = (Spinner) findViewById(R.id.semesterSpinner);
+        Spinner courseItems = (Spinner) findViewById(R.id.courseSpinner);
 
         // Get the name of the selected semester from the spinner.
-        String selectedCourse = sItems.getSelectedItem().toString();
+        int selectedSemester = (int)semesterItems.getSelectedItem();
+        String selectedCourse = courseItems.getSelectedItem().toString();
 
         /** Add the code to only do the next part when the button is selected. **/
         // Search the array list of courses in the selected semester to its index so that it can be
         // removed.
 
-        for(int i = 0; i < history.getSemesters().get(selectedSemester).getCourses().size(); i ++)
+        for(int i = 0; i < history.getSemesters().get(selectedSemester-1).getCourses().size(); i ++)
         {
-            if(history.getSemesters().get(selectedSemester).getCourses().get(i).getName() == selectedCourse)
-                history.getSemesters().get(selectedSemester).getCourses().remove(i);
+            if(history.getSemesters().get(selectedSemester-1).getCourses().get(i).getName().equals(selectedCourse))
+                history.getSemesters().get(selectedSemester-1).getCourses().remove(i);
         }
+
+        //Save sharedPref Person
+        SharedPreferences.Editor editor = sharedPref.edit();
+        gson = new Gson();
+        json = gson.toJson(person);
+        editor.putString("Person", json);
+        editor.apply();
 
         startActivity(new Intent(this, ScheduleActivity.class));
     }
